4a028607bad0a7395a740942bd87ac3a3562c605
Soushi Tanaka <wntsssbbsnws@yahoo.co.jp>
2022-01-02 21:03:45 +0900

    Add ListClass

--- CTAProject/UIComponent/ViewController/ListViewController.swift
+++ CTAProject/UIComponent/ViewController/ListViewController.swift
@@ -1,26 +1,111 @@
 
 import UIKit
-class ListViewController: UIViewController {
+import PKHUD
+import RxCocoa
+import RxDataSources
+import RxSwift
 
-    let headerView = TabBarControllerHeaderView()
-    let viewModel: ListViewModelType
+final class ListViewController: UIViewController {
+
+    private let headerView = TabBarControllerHeaderView()
+    private let searchView = TabBarControllerSearchView()
+    private let tableView = UITableView()
+    private var datasource: RxTableViewSectionedReloadDataSource<HotPepperResponseDataSource>?
+    private let disposeBag = DisposeBag()
+
+    private let viewModel: ListViewModelType
     init(viewModel: ListViewModelType) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
-        view.backgroundColor = .blue
+
+        // UI・layout
+        view.backgroundColor = .systemYellow
         view.addSubview(headerView)
+        view.addSubview(searchView)
+        view.addSubview(tableView)
+        tableView.register(UINib(nibName: "HotPepperTableViewCell", bundle: nil), forCellReuseIdentifier: "HotPepperTableViewCell")
+        tableView.backgroundColor = .systemGray6
+        datasource = RxTableViewSectionedReloadDataSource<HotPepperResponseDataSource>(
+            configureCell: { _, tableView, indexPath, items in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "HotPepperTableViewCell", for: indexPath) as! HotPepperTableViewCell
+                cell.setupCell(item: items)
+                //            cell.delegate = self
+                return cell
+            })
+        setLayout()
+
+        // Input
+        searchView.searchBar.rx.text.orEmpty
+            .bind(to: viewModel.input.searchTextInput)
+            .disposed(by: disposeBag)
+        searchView.searchBar.rx.searchButtonClicked
+            .subscribe(onNext: { [weak self] in
+                guard let me = self else { return }
+                viewModel.input.searchButtonTapped.onNext(me.searchView.searchBar.text ?? "")
+                me.view.endEditing(true)
+            }).disposed(by: disposeBag)
+
+        // Output
+        viewModel.output.validatedText
+            .bind(to: searchView.searchBar.rx.text)
+            .disposed(by: disposeBag)
+
+        viewModel.output.alert
+            .subscribe(onNext: { type in
+        }).disposed(by: disposeBag)
+
+        viewModel.output.hud
+            .observe(on: ConcurrentMainScheduler.instance)
+            .subscribe(onNext: { type in
+                HUD.show(type)
+            }).disposed(by: disposeBag)
+
+        viewModel.output.dismissHUD
+            .observe(on: ConcurrentMainScheduler.instance)
+            .subscribe(onNext: {
+                HUD.hide()
+            }).disposed(by: disposeBag)
+
+        viewModel.output.alert
+            .subscribe(onNext: { [weak self] alertType in
+                guard let me = self else { return }
+                switch alertType {
+                case .textCountOver:
+                    let alertView = AlertView(message: L10n.charactersExceeds50)
+                    me.showView(view: alertView)
+                default:
+                    break
+                }
+            }).disposed(by: disposeBag)
+
+        viewModel.output.datasource
+            .bind(to: tableView.rx.items(dataSource: datasource!))
+            .disposed(by: disposeBag)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func viewDidLayoutSubviews() {
+    private func setLayout() {
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.topAnchor),
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leftAnchor.constraint(equalTo: view.leftAnchor),
             headerView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: view.safeAreaInsets.top + 40)
+            headerView.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
+            searchView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            searchView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            searchView.heightAnchor.constraint(equalToConstant: searchView.heightConst)
+        ])
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: searchView.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
--- CTAProject/UILogic/ListViewModel.swift
+++ CTAProject/UILogic/ListViewModel.swift
@@ -1,2 +1,118 @@
 
-class ListViewModel: ListViewModelType {}
+import RxCocoa
+import RxDataSources
+import RxSwift
+import RxRelay
+import Unio
+import PKHUD
+
+protocol ListViewModelType: AnyObject {
+    var input: InputWrapper<ListViewModel.Input> { get }
+    var output: OutputWrapper<ListViewModel.Output> { get }
+}
+
+final class ListViewModel: UnioStream<ListViewModel>, ListViewModelType {
+    convenience init() {
+        self.init(input: Input(),
+                  state: State(),
+                  extra: Extra()
+        )
+    }
+    static func bind(from dependency: Dependency<Input, State, Extra>, disposeBag: DisposeBag) -> Output {
+
+        let input = dependency.inputObservables
+        let state = dependency.state
+
+        input.searchTextInput
+            .subscribe(onNext: { text in
+                print(text.count)
+                if text.count > 50 {
+                    state.alertType.accept(.textCountOver)
+                } else {
+                    state.validatedText.accept(text)
+                }
+            }).disposed(by: disposeBag)
+
+        input.searchButtonTapped
+            .flatMapLatest({ text -> Observable<Event<HotPepperResponse>> in
+                HotPepperRepository.search(keyValue: ["keyword": text]).materialize()
+            }).subscribe(onNext: { event in
+                switch event {
+                case .next(let response):
+                    state.datasource.accept([HotPepperResponseDataSource(items: response.results.shop)])
+                case .error:
+                    state.hud.accept(.error)
+                default:
+                    state.dismissHUD.accept(())
+                }
+            }).disposed(by: disposeBag)
+
+        // MARK: - HUD表示は 0.7秒後 にdismissする
+        state.hud
+            .delay(RxTimeInterval.milliseconds(700),
+                   scheduler: ConcurrentMainScheduler.instance)
+            .subscribe(onNext: { _ in
+                state.dismissHUD.accept(())
+            }).disposed(by: disposeBag)
+
+        input.saveFavorite.subscribe(onNext: { shop in
+            let object = ShopObject(shop: shop)
+            RealmManager.addEntity(object: object) { status in
+                switch status {
+                case .success:
+                    state.hud.accept(.success)
+                case .error:
+                    state.hud.accept(.error)
+                }
+            }
+            print("EntityList:\(RealmManager.getEntityList(type: ShopObject.self))")
+        }).disposed(by: disposeBag)
+
+        input.deleteObject.subscribe(onNext: { objectName in
+            RealmManager.deleteOneObject(type: ShopObject.self, name: objectName) { status in
+                switch status {
+                case .success:
+                    state.hud.accept(.success)
+                case .error:
+                    state.hud.accept(.error)
+                }
+            }
+        }).disposed(by: disposeBag)
+
+        return Output(alert: state.alertType.asObservable(),
+                      validatedText: state.validatedText.asObservable(),
+                      datasource: state.datasource.asObservable(),
+                      hud: state.hud.asObservable(),
+                      dismissHUD: state.dismissHUD.asObservable()
+        )
+    }
+
+}
+extension ListViewModel {
+
+    struct Input: InputType {
+        let searchTextInput = PublishRelay<String>()
+        let searchButtonTapped = PublishRelay<String>()
+        let saveFavorite = PublishRelay<Shop>()
+        let deleteObject = PublishRelay<String>()
+    }
+
+    struct Output: OutputType {
+        let alert: Observable<AlertType>
+        let validatedText: Observable<String>
+        let datasource: Observable<[HotPepperResponseDataSource]>
+        let hud: Observable<HUDContentType>
+        let dismissHUD: Observable<Void>
+    }
+
+    struct Extra: ExtraType {
+    }
+
+    struct State: StateType {
+        let alertType = PublishRelay<AlertType>()
+        let validatedText = BehaviorRelay<String>(value: "")
+        let datasource = BehaviorRelay<[HotPepperResponseDataSource]>(value: [])
+        let hud = PublishRelay<HUDContentType>()
+        let dismissHUD = PublishRelay<Void>()
+    }
+}
