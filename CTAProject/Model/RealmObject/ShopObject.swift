c92bf97b0127ffe36e38093b2261759a616d97d8
Soushi Tanaka <wntsssbbsnws@yahoo.co.jp>
2022-01-02 20:54:47 +0900

    Add RealmManager

---
+++ CTAProject/Model/RealmObject/ShopObject.swift
@@ -0,0 +1,36 @@
+
+import UIKit
+import Realm
+import RealmSwift
+
+@objcMembers
+class ShopObject: Object {
+
+    dynamic var name = ""
+    dynamic var budgetName = ""
+    dynamic var genre = ""
+    dynamic var station = ""
+    dynamic var logoImage = ""
+    dynamic var lat = 0.0
+    dynamic var lng = 0.0
+
+    convenience init(shop: Shop) {
+        self.init()
+        name = shop.name
+        budgetName = shop.budget?.name ?? ""
+        genre = shop.genre.name
+        station = shop.stationName ?? ""
+        lat = shop.lat
+        lng = shop.lng
+
+        if let logoImage = shop.logoImage {
+            self.logoImage = "\(logoImage)"
+        }
+
+
+    }
+
+    override class func primaryKey() -> String? {
+        return "name"
+    }
+}
---
+++ CTAProject/Model/RealmStatus.swift
@@ -0,0 +1,5 @@
+
+enum RealmStatus {
+    case success
+    case error
+}
