3e544464ea2a28ab40544bc3d6e21053fac27bf5
Soushi Tanaka <wntsssbbsnws@yahoo.co.jp>
2022-01-05 00:17:02 +0900

    Fix SchedulerInstance

--- CTAProject/UIComponent/ViewController/ListViewController.swift
+++ CTAProject/UIComponent/ViewController/ListViewController.swift
@@ -36,7 +36,7 @@ final class ListViewController: UIViewController {
 
         // Input
         searchView.searchBar.searchTextField.rx.controlEvent([.editingChanged])
-            .observe(on: MainScheduler.asyncInstance)
+            .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] in
                 guard let me = self else { return }
                 let text = me.searchView.searchBar.text ?? ""
